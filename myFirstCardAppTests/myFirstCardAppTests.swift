b89ab2a8304bf87dd635706737a8c0b23bc4353a
Balazs127 <k2216408@kingston.ac.uk>
2024-12-06 13:36:45 +0000

    Initial Commit

---
+++ myFirstCardAppTests/myFirstCardAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstCardAppTests.swift
+//  myFirstCardAppTests
+//
+//  Created by Kis, Balazs on 06/12/2024.
+//
+
+import Testing
+@testable import myFirstCardApp
+
+struct myFirstCardAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
